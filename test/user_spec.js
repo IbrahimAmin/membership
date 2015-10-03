dcdb3480fb9f3b83ac769626e6a1c7548d16ea43
Ibrahim Ahmed <ibrahimtorbany.dev@outlook.com>
2015-10-03 14:20:50 +0200

    user_spec

--- test/user_spec.js
+++ test/user_spec.js
@@ -1,4 +1,5 @@
 var should = require("should");
+var User = require("../models/user");
 
 describe("User", function () {
     describe("defaults", function () {
@@ -8,16 +9,26 @@ describe("User", function () {
             user = new User({ email: "ibrahim@biztek.io" });
         });
 
-        it("email is ibrahim@biztek.io");
-        it("has an authentication token");
-        it("has a pending status");
-        it("has a created date");
-        it("has a signInCount of 0");
-        it("has lastLogin");
-        it("has currentLogin");
-        it("has a sessionToken");
-        it("has an authentication token");
-        it("has a reminderToken");
-        it("has a reminder sent date");
+        it("email is ibrahim@biztek.io", function () {
+            user.email.should.equal("ibrahim@biztek.io");
+        });
+        it("has an authentication token", function () {
+            user.authenticationToken.should.be.defined;
+        });
+        it("has a pending status", function () {
+            user.status.should.equal("pending");
+        });
+        it("has a created date", function () {
+            user.createdAt.should.be.defined;
+        });
+        it("has a signInCount of 0", function () {
+            user.signInCount.should.equal("0");
+        });
+        it("has lastLogin", function () {
+            user.lastLogin.should.be.defined;
+        });
+        it("has currentLogin", function () {
+            user.currentLoginAt.should.be.defined;
+        });
     });
 });
